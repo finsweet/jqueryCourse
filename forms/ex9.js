79ba9a5c0f1f06d68134543beefff8b1f7f7ddc6
Brian Musuku <brayanfinsweet@github.com>
2021-03-14 13:20:55 +0300

    demo

---
+++ forms/ex9.js
@@ -0,0 +1,17 @@
+// Fill the form and the code will validate if everything is perfect. Press run to try again.
+if ($("#name").val() === "") {
+  $(".message").css("display", "flex").css("color", "red");
+  $(".message").text("Please provide your name!");
+} else if ($("#email").val() === "") {
+  $(".message").css("display", "flex").css("color", "red");
+  $(".message").text("Please enter your email!");
+} else if (!$("#checkbox").prop("checked")) {
+  $(".message").css("display", "flex").css("color", "red");
+  $(".message").text("Please check the checkbox!");
+} else if ($("#field").val() === "") {
+  $(".message").css("display", "flex").css("color", "red");
+  $(".message").text("Please select an option from the dropdown!");
+} else {
+  $(".message").css("display", "flex").css("color", "green");
+  $(".message").text("All good!");
+}
