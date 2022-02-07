7ee798c9bd8667a1934e8c124d693a3bfeb8497b
Brian Musuku <brayanfinsweet@github.com>
2022-02-08 00:55:47 +0300

    bug: prevent form submission

--- forms/ex7.js
+++ forms/ex7.js
@@ -2,10 +2,10 @@
 $("#email-form").submit(function (e) {
   if ($("#name").val() === "") {
     alert("Please provide your name!");
-    e.stopPropagation();
+    e.preventDefault();
   } else if ($("#field").val() === "") {
     alert("Please select an option from the dropdown!");
-    e.stopPropagation();
+    e.preventDefault();
   } else {
   }
 });
