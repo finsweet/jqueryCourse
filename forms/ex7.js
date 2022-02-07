6c050eaf585ee324b2917820c657d39ff3229a56
Brian Musuku <brayanfinsweet@github.com>
2022-02-08 01:03:47 +0300

    refactor: remove useless return

--- forms/ex7.js
+++ forms/ex7.js
@@ -3,11 +3,9 @@ $("#email-form").submit(function (e) {
   if ($("#name").val() === "") {
     alert("Please provide your name!");
     e.stopPropagation();
-    return false
   } else if ($("#field").val() === "") {
     alert("Please select an option from the dropdown!");
     e.stopPropagation();
-    return false
   } else {
   }
 });
