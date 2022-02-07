be3486cd9b08d759c2549f681a645a8e105e1df2
Brian Musuku <brayanfinsweet@github.com>
2022-02-08 00:59:14 +0300

    bug: add return to prevent wf-page reload

--- forms/ex7.js
+++ forms/ex7.js
@@ -3,9 +3,11 @@ $("#email-form").submit(function (e) {
   if ($("#name").val() === "") {
     alert("Please provide your name!");
     e.stopPropagation();
+    return
   } else if ($("#field").val() === "") {
     alert("Please select an option from the dropdown!");
     e.stopPropagation();
+    return
   } else {
   }
 });
