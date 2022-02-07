ff9670b8b39fa5441037ded3b6e9278b236e99ad
Brian Musuku <brayanfinsweet@github.com>
2022-02-08 00:54:15 +0300

    bug: fix form submission prevention listing

--- forms/ex7.js
+++ forms/ex7.js
@@ -1,9 +1,11 @@
 // Press the submit button.
-$("#email-form").submit(function () {
+$("#email-form").submit(function (e) {
   if ($("#name").val() === "") {
     alert("Please provide your name!");
+    e.preventDefault();
   } else if ($("#field").val() === "") {
     alert("Please select an option from the dropdown!");
+    e.preventDefault();
   } else {
   }
 });
