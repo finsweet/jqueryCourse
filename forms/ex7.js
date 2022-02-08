f8222316c7ed83016ec65685f4b37375ca005686
Brian Musuku <brayanfinsweet@github.com>
2022-02-08 21:15:24 +0300

    bug: prevent page from refreshing button sumit

--- forms/ex7.js
+++ forms/ex7.js
@@ -3,9 +3,11 @@ $("#email-form").submit(function (e) {
   if ($("#name").val() === "") {
     alert("Please provide your name!");
     e.stopPropagation();
+    e.preventDefault();
   } else if ($("#field").val() === "") {
     alert("Please select an option from the dropdown!");
     e.stopPropagation();
+    e.preventDefault();
   } else {
   }
 });
