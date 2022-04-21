0bea3d0059588a321e134d69ba3484b8cd631f19
Brian Musuku <brayanfinsweet@github.com>
2022-04-21 14:45:14 +0300

    fix: add comment

--- forms/ex14.js
+++ forms/ex14.js
@@ -1,4 +1,4 @@
-// Prevent submit you agree
+// Prevent submit till you check agree.
 $("#email-form").submit(function (event) {
   if (!$("#checkbox").prop("checked")) {
     $(".message").css("display", "flex").css("color", "red");
