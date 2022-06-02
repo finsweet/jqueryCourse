5b3196220e75d54cb3cdaf821040a7620f50d4e1
xlvisben <elvisben8@gmail.com>
2022-06-03 01:22:32 +0300

    Date demo fix

--- jsobjects/ex7.js
+++ jsobjects/ex7.js
@@ -1,5 +1,5 @@
 // various date object examples
 new Date();
-new Date(milliseconds);
-new Date(datestring);
+new Date(milliseconds); // eg new Date(1654208111594)
+new Date(datestring); // eg new Date('Fri Jun 03 2022 01:15:43')
 new Date(year,month,date[ hour,minute,second,millisecond ]);
